129abc0f0cca63b4c97138fc714bcfd6689ce0fa
AnoushaBaig <164419082+AnoushaBaig@users.noreply.github.com>
2024-04-25 20:36:29 -0700

    push my assaigment

---
+++ README.md
@@ -0,0 +1,1 @@
+"# assigment_1till10" 
---
+++ main.ts
@@ -0,0 +1,61 @@
+// Task 01
+// let personName : string ="Eric"
+// let message : string = "would you like to learn some python today?"
+// console.log(`hello ${personName} ${message}`);
+
+// task 02
+// let personName :string ="Anousha Baig"
+
+// console.log("lowercase:", personName .toLowerCase());
+
+// console.log("Uppercase:", personName.toUpperCase());
+
+// console.log("tittlecase:",personName.replace(/\bw/g,c => c.toUpperCase()));
+
+// task 04
+let authorName :string="Albert Einstine";
+let quote : string ="A person who never made a mistake never tried anything new."
+console.log(`${authorName} once said, ${quote}`);
+
+// task 05
+let famouspersonName :string ="Albert Einstine";
+let massage:string="A person who never made a mistake never tried anything new."
+console.log(`${famouspersonName} once said ${massage}`);
+
+// task 06
+let personName :string  = "\n\t Anousha Baig /t/n";
+console.log(personName);
+let  stripped :string = personName .trim();
+console.log(stripped);
+
+// task 07 // 08
+console.log(5+3);
+console.log(11 -3);
+console.log(4 *2);
+console.log(16 / 2);
+
+// task 09
+let favouriteNumber = 6;
+console.log(`My favourite Number ${favouriteNumber}` );
+
+// task 10
+// my name is Anousha 
+// 08 -04 -2024
+// thi is a simple multiplication
+console.log(5*2);
+// this is a simple rogramm hello world
+console.log(`hello world simple program`);
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
